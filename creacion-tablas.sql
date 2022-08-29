e26bed6a9f7f0b8628c659ecf58812456d2f710b
2730538920101 <carlosmartz1995@gmail.com>
2022-08-29 12:26:15 -0600

    (add) Creacion de tablas 2 y modelo

--- creacion-tablas.sql
+++ creacion-tablas.sql
@@ -37,17 +37,37 @@ CREATE TABLE CLIENTES(
     FOREIGN KEY (fk_id_pais) REFERENCES PAISES(id_pais)
 );
 
-CREATE TABLE ORDENES(
-    id_orden INT NOT NULL PRIMARY KEY ,
-    linea_orden INT NOT NULL PRIMARY KEY ,
+CREATE TABLE TEMPORAL_ORDENES(
+    id_orden INT NOT NULL ,
+    linea_orden INT NOT NULL ,
     fecha_orden DATE NOT NULL ,
-    fk_id_cliente INT NOT NULL ,
-    fk_id_vendedor INT NOT NULL ,
-    fk_id_producto INT NOT NULL ,
-    FOREIGN KEY (fk_id_cliente) REFERENCES CLIENTES(id_cliente),
-    FOREIGN KEY (fk_id_vendedor) REFERENCES VENDEDORES(id_vendedor),
-    FOREIGN KEY (fk_id_producto) REFERENCES  PRODUCTOS(id_producto)
+    id_cliente INT NOT NULL ,
+    id_vendedor INT NOT NULL ,
+    id_producto INT NOT NULL,
+    cantidad INT NOT NULL
 );
+CREATE TABLE ORDENES(
+  id_orden INT NOT NULL PRIMARY KEY ,
+  fecha_orden DATE NOT NULL ,
+  fk_id_cliente INT NOT NULL ,
+  FOREIGN KEY  (fk_id_cliente) REFERENCES CLIENTES(id_cliente)
+);
+
+CREATE TABLE DETALLE_ORDEN(
+    id_detalle INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    linea_orden INT NOT NULL ,
+    fk_id_producto INT NOT NULL ,
+    cantidad INT NOT NULL ,
+    fk_id_vendedor INT NOT NULL ,
+    fk_id_orden INT NOT NULL ,
+    FOREIGN KEY  (fk_id_producto) REFERENCES  PRODUCTOS(id_producto),
+    FOREIGN KEY (fk_id_vendedor) REFERENCES VENDEDORES(id_vendedor),
+    FOREIGN KEY (fk_id_orden) REFERENCES ORDENES(id_orden)
+);
+
+
+
+
 
 
 
